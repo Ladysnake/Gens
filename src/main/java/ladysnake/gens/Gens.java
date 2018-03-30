0a217745d8999cc13ee6d696407732a2767fd3f0
Fabien <redstoneinfire@gmail.com>
2018-03-30 19:55:48 +0200

    Let there be mod

---
+++ src/main/java/ladysnake/gens/Gens.java
@@ -0,0 +1,8 @@
+package ladysnake.gens;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Gens.MOD_ID, name = "Gens", version = "@VERSION@")
+public class Gens {
+    public static final String MOD_ID = "gens";
+}
---
+++ src/main/java/ladysnake/gens/init/ModItems.java
@@ -0,0 +1,8 @@
+package ladysnake.gens.init;
+
+import ladysnake.gens.Gens;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+@GameRegistry.ObjectHolder(Gens.MOD_ID)
+public class ModItems {
+}
