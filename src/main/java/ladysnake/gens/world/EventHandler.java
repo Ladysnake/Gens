624116a6398edbcd90f8f33ee033c2260992978f
Erlend Åmdal <erlend@aamdal.com>
2018-03-30 20:08:53 +0200

    Add world gen event handler

---
+++ src/main/java/ladysnake/gens/world/EventHandler.java
@@ -0,0 +1,10 @@
+package ladysnake.gens.world;
+
+import net.minecraftforge.event.terraingen.InitMapGenEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class EventHandler {
+    @SubscribeEvent
+    public void onInitMapGen(InitMapGenEvent event) {
+    }
+}
